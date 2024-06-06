3c058a1faf4dae8ae17c96424a2958747f847c7b
Henry Zongaro <zongaro@ca.ibm.com>
2024-06-06 14:21:49 -0700

    Younes patch - avoids special Unsafe static field handling for fields of type boolean, byte, char, short or Object.

--- runtime/compiler/optimizer/InlinerTempForJ9.cpp
+++ runtime/compiler/optimizer/InlinerTempForJ9.cpp
@@ -1201,8 +1201,12 @@ TR_J9InlinerPolicy::createUnsafePutWithOffset(TR::ResolvedMethodSymbol *calleeSy
 
    static char *useAbsoluteAddressesForStatics = feGetEnv("TR_UseAbsoluteAddressesForStatics");
    bool conversionNeeded = (comp()->fe()->dataTypeForLoadOrStore(type) != type);
+   bool typeAllowsDirectAccess = type == TR::Int32 ||
+                                 type == TR::Int64 ||
+                                 type == TR::Float ||
+                                 type == TR::Double;
 
-   bool onlyNeedsDirectAccess = ((useAbsoluteAddressesForStatics != NULL) && !conversionNeeded);
+   bool onlyNeedsDirectAccess = ((useAbsoluteAddressesForStatics != NULL) && !conversionNeeded && typeAllowsDirectAccess);
 
 //   if (!onlyNeedsDirectAccess)
       {
@@ -1539,8 +1543,11 @@ TR_J9InlinerPolicy::createUnsafeGetWithOffset(TR::ResolvedMethodSymbol *calleeSy
 
    static char *useAbsoluteAddressesForStatics = feGetEnv("TR_UseAbsoluteAddressesForStatics");
    bool conversionNeeded = (comp()->fe()->dataTypeForLoadOrStore(type) != type);
-
-   bool onlyNeedsDirectAccess = ((useAbsoluteAddressesForStatics != NULL) && !conversionNeeded);
+   bool typeAllowsDirectAccess = type == TR::Int32 ||
+                                 type == TR::Int64 ||
+                                 type == TR::Float ||
+                                 type == TR::Double;
+   bool onlyNeedsDirectAccess = ((useAbsoluteAddressesForStatics != NULL) && !conversionNeeded && typeAllowsDirectAccess);
 
    // Preserve null check on the unsafe object
    TR::TransformUtil::separateNullCheck(comp(), callNodeTreeTop, tracer()->debugLevel());
