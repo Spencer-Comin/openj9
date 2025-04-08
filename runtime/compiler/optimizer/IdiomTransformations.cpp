52b64e56be77701b620a40cc908471d414aa4ff5
Spencer Comin <spencer.comin@ibm.com>
2025-04-08 13:42:43 -0400

    cut const

--- runtime/compiler/optimizer/IdiomTransformations.cpp
+++ runtime/compiler/optimizer/IdiomTransformations.cpp
@@ -8420,7 +8420,7 @@ CISCTransform2ArraySet(TR_CISCTransformer *trans)
       cfg->addNode(blockEnd);
       }
 
-   TR::SymbolReference const *arraysetSymRef = comp->getSymRefTab()->findOrCreateArraySetSymbol();
+   TR::SymbolReference *arraysetSymRef = comp->getSymRefTab()->findOrCreateArraySetSymbol();
    if (storeCount == 1)
       {
       auto nodes = findArraySetNodes(iteratorStores.getFirst());
