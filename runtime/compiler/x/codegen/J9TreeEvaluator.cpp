0d400ece388479b35de8345a4b90cb01a48af126
Spencer Comin <Spencer.Comin@ibm.com>
2025-11-27 16:01:29 -0500

    Pin to rax

--- runtime/compiler/x/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/x/codegen/J9TreeEvaluator.cpp
@@ -1719,7 +1719,7 @@ static TR::Register * generate2DArrayWithInlineAllocators(TR::Node *node, TR::Co
    // done, OOL helper will return to this point
    TR::RegisterDependencyConditions *deps = generateRegisterDependencyConditions(0, 13, cg);
    deps->addPostCondition(tempReg, TR::RealRegister::NoReg, cg);
-   deps->addPostCondition(spinePtrReg, TR::RealRegister::NoReg, cg);
+   deps->addPostCondition(spinePtrReg, TR::RealRegister::eax, cg);
    deps->addPostCondition(leafPtrReg, TR::RealRegister::NoReg, cg);
 
    deps->addPostCondition(nDimsReg, TR::RealRegister::NoReg, cg);
@@ -1768,7 +1768,10 @@ static TR::Register * generate2DArrayWithInlineAllocators(TR::Node *node, TR::Co
 
    // now that the array is properly allocated, move into a collected reference register
    TR::Register *returnReg = cg->allocateCollectedReferenceRegister();
-   generateRegRegInstruction(TR::InstOpCode::MOV8RegReg, node, returnReg, spinePtrReg, cg);
+   TR::RegisterDependencyConditions  *deps2 = generateRegisterDependencyConditions(0, 1, cg);
+   deps2->addPostCondition(returnReg, TR::RealRegister::eax, cg);
+   generateRegRegInstruction(TR::InstOpCode::MOV8RegReg, node, returnReg, spinePtrReg, deps2, cg);
+   cg->stopUsingRegister(spinePtrReg);
 
    TR_Debug *debug = cg->getDebug();
    if (debug)
